d36bf1b001932e96902694d4d16daf145d7da855
h4ck3r-04 <mr.rudradityathakur@gmail.com>
2024-03-21 13:28:30 +0530

    defined usage of protocols

--- fpassword.cpp
+++ fpassword.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 #include <getopt.h>
-using namespace std;
+
+void usage_oracle(const char *service);
+void usage_oracle_listener(const char *service);
+void usage_cvs(const char *service);
+void usage_xmpp(const char *service);
+void usage_pop3(const char *service);
+void usage_rdp(const char *service);
+void usage_s7_300(const char *service);
+void usage_nntp(const char *service);
+void usage_imap(const char *service);
+void usage_smtp_enum(const char *service);
+void usage_smtp(const char *service);
+void usage_svn(const char *service);
+void usage_ncp(const char *service);
+void usage_firebird(const char *service);
+void usage_mysql(const char *service);
+void usage_mongodb(const char *service);
+void usage_irc(const char *service);
+void usage_postgres(const char *service);
+void usage_telnet(const char *service);
+void usage_sapr3(const char *service);
+void usage_sshkey(const char *service);
+void usage_cisco_enable(const char *service);
+void usage_cisco(const char *service);
+void usage_ldap(const char *service);
+void usage_smb(const char *service);
+void usage_http_form(const char *service);
+void usage_http_proxy(const char *service);
+void usage_http_proxy_urlenum(const char *service);
+void usage_snmp(const char *service);
+void usage_http(const char *service);
+void usage_smb2(const char *service);
 
 void help(int32_t ext){
-  cout << "help" << endl;
+  std::cout << "help" << std::endl;
 }
 
 int main(int argc, char* argv[]) {
